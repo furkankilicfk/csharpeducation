be0cd9880679acc4f49535017abd02570a159eb9
Furkan Kılıç <78054012+furkankilicfk@users.noreply.github.com>
2023-03-23 23:57:58 +0300

    Encapsulation Example

---
+++ EncapsulationExample/EncapsulationExample/Personel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample
+{
+    public class Personel
+    {
+        private string tckimlikno;
+
+        public string TCKIMLIKNO
+        {
+            get
+            {
+                return tckimlikno.Substring(0,5)+"******";
+            }
+
+            set 
+            {
+                bool kontrol = false;
+
+                if(value.Length==11) //kullanıcının girdiği değeri value anahtar kelimesiyle yakaladım. value içerisinde bazı metotlar ve propertyler tanımlanmıştır.
+                {
+                    for(int i = 0; i<value.Length; i++)
+                    {
+                        bool sayiMi = char.IsNumber(value[i]); //char'ın içerisinde bazı hazır metotlar var. senden char veri tipinde, yani bir karakter istiyorum, sana boolean döndürürüm.
+
+                        if (sayiMi)
+                        {
+                            //Yapmam gereken bir iş yok
+                        }
+                        else
+                        {
+                            kontrol = true; //dipnot: kontrol değişkenimiz olmasa algoritmayı yapamayız. true'ya çekip aşağıda kullanıyoruz.
+                            break;
+                        }
+                    }
+                    if(kontrol)
+                    {
+                        Console.WriteLine("LÜTFEN TC NO İÇİ SADECE SAYI GİRİNİZ.");
+                    }
+                    else
+                    {
+                        tckimlikno = value; // eğer herşey doğruysa değişkenime value anahtar kelimesini atıyorum.
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("TC NO 11 KARAKTER OLMAK ZORUNDADIR");
+                }
+            }
+            
+        }
+    }
+}
---
+++ EncapsulationExample/EncapsulationExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel pers = new Personel();
+            pers.TCKIMLIKNO = "12345678914";
+
+            Console.WriteLine("TC Kimlik No: " + pers.TCKIMLIKNO);
+
+            Console.ReadKey();
+        }
+    }
+}
